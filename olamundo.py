72fc4946df74a7ffc7d4e994e004aa05852e5f2e
Pedro Walter <pedrowalter511@gmail.com>
2026-05-05 21:07:39 -0300

    input de idade

--- olamundo.py
+++ olamundo.py
@@ -1,3 +1,7 @@
 nome = input("digite seu nome: ")
 print (f "olá {nome}!")
-
+idade = int(input(digite sua idade: ))
+if idade >= 18:
+    print("você é maior de idade. ")
+else:
+    print("você é menor de idade." )    
