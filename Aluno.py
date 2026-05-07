c418e08679a7119ac662946cd7a82e003927cf40
Pedro Walter <pedrowalter511@gmail.com>
2026-05-07 20:06:06 -0300

    nova classe

---
+++ Aluno.py
@@ -0,0 +1,9 @@
+class Aluno():
+def __init__(self, nome, email, senha telefone):
+        self.nome = nome,
+        self.senha = senha,
+        self.email = email,
+        self.telefone = telefone 
+
+        def login(email, senha):
+            print("login realizado com sucesso!")
